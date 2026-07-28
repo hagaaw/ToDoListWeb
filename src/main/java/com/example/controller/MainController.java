6af369d8f88fe31a4bef3c113dc06dc3c830d2db
Ilya Yahnis <ilavoroev@gmail.com>
2026-07-29 01:31:50 +0300

    add service

--- src/main/java/com/example/controller/MainController.java
+++ src/main/java/com/example/controller/MainController.java
@@ -1,21 +1,24 @@
 package com.example.controller;
 
 
-import com.example.entity.StatusTask;
-import com.example.entity.TaskEntity;
 import com.example.repository.TaskRepository;
+import com.example.service.ServiceForMainController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.List;
 
 @Controller
 public class MainController {
-    @Autowired
     private TaskRepository taskRepository;
-
+    private ServiceForMainController service;
+    @Autowired
+    public MainController(TaskRepository taskRepository,
+                          ServiceForMainController service){
+        this.taskRepository = taskRepository;
+        this.service = service;
+    }
 
     @GetMapping("/home")
     private String getMain(Model model){
