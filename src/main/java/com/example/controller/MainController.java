4520d4d590233951fa414a3ce7f96c53e69f73a6
Ilya Yahnis <ilavoroev@gmail.com>
2026-07-29 00:33:36 +0300

    add task entity

--- src/main/java/com/example/controller/MainController.java
+++ src/main/java/com/example/controller/MainController.java
@@ -2,12 +2,15 @@ package com.example.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class MainController {
     @GetMapping("/home")
-    private String getMain(){
+    private String getMain(Model model){
         return "main-page.html";
     }
 
