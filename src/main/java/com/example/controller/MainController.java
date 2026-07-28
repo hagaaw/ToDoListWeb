3a56cfb9c5c6752cf6192d042539f337ee9f567f
Ilya Yahnis <ilavoroev@gmail.com>
2026-07-29 01:27:51 +0300

    add new entity

--- src/main/java/com/example/controller/MainController.java
+++ src/main/java/com/example/controller/MainController.java
@@ -1,6 +1,10 @@
 package com.example.controller;
 
 
+import com.example.entity.StatusTask;
+import com.example.entity.TaskEntity;
+import com.example.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,10 +13,16 @@ import java.util.List;
 
 @Controller
 public class MainController {
+    @Autowired
+    private TaskRepository taskRepository;
+
+
     @GetMapping("/home")
     private String getMain(Model model){
         return "main-page.html";
     }
+
+
 
 
 }
---
+++ src/main/java/com/example/entity/TaskEntity.java
@@ -0,0 +1,52 @@
+package com.example.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tasks")
+public class TaskEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "status_task", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private StatusTask statusTask;
+
+
+    public TaskEntity(String name, StatusTask statusTask) {
+        this.name = name;
+        this.statusTask = statusTask;
+    }
+
+    public TaskEntity() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public StatusTask getStatusTask() {
+        return statusTask;
+    }
+
+    public void setStatusTask(StatusTask statusTask) {
+        this.statusTask = statusTask;
+    }
+}
