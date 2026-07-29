2f2a633588163185a8d77222c299e570dac5b240
Ilya Yahnis <ilavoroev@gmail.com>
2026-07-29 19:26:45 +0300

    del unnecessary dependencies

--- src/main/java/com/example/controller/MainController.java
+++ src/main/java/com/example/controller/MainController.java
@@ -1,9 +1,7 @@
 package com.example.controller;
 
 
-import com.example.entity.StatusTask;
 import com.example.entity.TaskEntity;
-import com.example.repository.TaskRepository;
 import com.example.service.ServiceForMainController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
