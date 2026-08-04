93e78b2986ed97c22b78e98f7a9ea37a5c34d29e
Ilya Yahnis <ilavoroev@gmail.com>
2026-08-04 13:31:24 +0300

    add config class

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -1,24 +1,36 @@
 package com.example.config;
 
-
 import com.example.entity.UserRole;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig{
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http){
+    public SecurityFilterChain filterChain(HttpSecurity http){
         return http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth->auth
-                        .requestMatchers("/", "/main", "/error-page","/registration", "/login").permitAll()
+                .authorizeHttpRequests(auth->auth.
+                        requestMatchers("/", "/main", "/registration","/login", "/error-page","/css/**").permitAll()
                         .requestMatchers("/account/**").hasAnyRole(UserRole.USER.name(), UserRole.ADMIN.name())
-                        .requestMatchers("/admin/**").hasAnyRole(UserRole.ADMIN.name())).build();
+                        .requestMatchers("/admin/**").hasAnyRole(UserRole.ADMIN.name()))
+                .formLogin(form->form
+                        .loginPage("/login").defaultSuccessUrl("/account").permitAll())
+                .logout(logout->logout
+                        .logoutSuccessUrl("/main").permitAll())
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
